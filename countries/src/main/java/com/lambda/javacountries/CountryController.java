87674fae0361cadea8614978c66dbee5527381a2
deejayeaster@live.com <deejayeaster@live.com>
2019-09-10 01:01:34 -0400

    set up controller

---
+++ countries/src/main/java/com/lambda/javacountries/CountryController.java
@@ -0,0 +1,14 @@
+package com.lambda.javacountries;
+
+public class CountryController {
+
+    // localhostLL8080/names/all
+
+    // /names/start/{letter}
+
+    // /names/size/{number}
+
+    // /population/size/{people}
+
+    // /population/min
+}
